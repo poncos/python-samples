ff70875c0a681007f7988f6419770665e8c7b9ee
Esteban Collado <esteban.collado@pm.me>
2024-07-10 16:23:36 +0100

    update sql statements

--- sample_python_rest/datastore/jobs_db.py
+++ sample_python_rest/datastore/jobs_db.py
@@ -14,18 +14,18 @@ class JobsDb:
         self.__create_job_stm = """insert into jobs
         (group_id, type, status, description)
         values
-        ('%s', '%s', 'PENDING', '%s') 
+        (?, ?, 'PENDING', ?) 
         """
-        #RETURNING id,group_id,type,description,status,created_at,started_at,finished_at)
+        # RETURNING id,group_id,type,description,status,created_at,started_at,finished_at)
 
         self.__list_jobs_stm = """
         select job_id, group_id, type, description, status, error_msg, created_at, started_at, finished_at
-        from jobs
+        from jobs where group_id=?
         """
 
         self.__select_job_stm = """
             select job_id,group_id,type,description,status,created_at,started_at,finished_at
-            from jobs where job_id=%d
+            from jobs where job_id=?
         """
 
         self.__db_con = None
@@ -34,39 +34,31 @@ class JobsDb:
         connection = self.__get_db_connection()
 
         cursor = connection.cursor()
-        statement = self.__create_job_stm % (group_id, job_type, description)
-        print(f"Running statement {statement}")
-        cursor.execute(statement)
+        params = (group_id, job_type, description)
+        cursor.execute(self.__create_job_stm, params)
         connection.commit()
 
         # Retrieve the ID of the inserted row
         last_row_id = cursor.lastrowid
-        print(f"Record created: {last_row_id}")
         record = self.__get_job_record(last_row_id, cursor)
-        print(f"Record created: {record}")
-
         return Job(id=record[0], group_id=record[1], type=record[2],
                    description=record[3], status=record[4], error_msg=record[5], created_at=record[6],
                    started_at=None, finished_at=None)
 
-    def __get_job_record(self, id, cursor):
-        get_stm = self.__select_job_stm % id
-
-        print(f"Running statement: {get_stm}")
-        cursor.execute(get_stm)
+    def __get_job_record(self, job_id, cursor):
+        cursor.execute(self.__select_job_stm, (job_id, ))
         return cursor.fetchone()
 
     def list_jobs(self, group_id) -> List[Job]:
         connection = self.__get_db_connection()
 
         cursor = connection.cursor()
-        cursor.execute(self.__list_jobs_stm)
+        cursor.execute(self.__list_jobs_stm, (group_id, ))
         records = cursor.fetchall()
 
         jobs = [None] * len(records)
         row_number = 0
 
-        print(f"Jobs returned: {records}")
         # try to substitute this look with list comprehension
         for row in records:
             jobs[row_number] = Job(id=row[0], group_id=row[1], type=row[2], description=row[3], status=row[4],
@@ -74,7 +66,6 @@ class JobsDb:
             row_number += 1
 
         cursor.close()
-        print(f"returning jobs: {jobs}")
         return jobs
 
     def __get_db_connection(self):
